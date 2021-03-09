a31345df924d6840b1a74eabf561bb2ba97f75b7
Nolan Neilson <NeilNola532@GoTVDSB.ca>
2021-03-08 22:48:55 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Part_8___Hangman
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsedLetters = new System.Windows.Forms.Label();
+            this.lstUsedLetters = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescrition = new System.Windows.Forms.Label();
+            this.imgHangman = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUsedLetters
+            // 
+            this.lblUsedLetters.AutoSize = true;
+            this.lblUsedLetters.Font = new System.Drawing.Font("Paradox Mosaic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsedLetters.Location = new System.Drawing.Point(341, 86);
+            this.lblUsedLetters.Name = "lblUsedLetters";
+            this.lblUsedLetters.Size = new System.Drawing.Size(149, 22);
+            this.lblUsedLetters.TabIndex = 1;
+            this.lblUsedLetters.Text = "Used letters";
+            // 
+            // lstUsedLetters
+            // 
+            this.lstUsedLetters.FormattingEnabled = true;
+            this.lstUsedLetters.Location = new System.Drawing.Point(369, 114);
+            this.lstUsedLetters.Name = "lstUsedLetters";
+            this.lstUsedLetters.Size = new System.Drawing.Size(86, 160);
+            this.lstUsedLetters.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(357, 319);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnGuess.Font = new System.Drawing.Font("Proxima Nova Lt", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnGuess.Location = new System.Drawing.Point(404, 319);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(71, 31);
+            this.btnGuess.TabIndex = 4;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(12, 383);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(404, 58);
+            this.lblWord.TabIndex = 5;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Paradox Mosaic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(121, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(252, 50);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "HangMan!";
+            // 
+            // lblDescrition
+            // 
+            this.lblDescrition.Font = new System.Drawing.Font("Proxima Nova Lt", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescrition.Location = new System.Drawing.Point(9, 59);
+            this.lblDescrition.Name = "lblDescrition";
+            this.lblDescrition.Size = new System.Drawing.Size(471, 27);
+            this.lblDescrition.TabIndex = 7;
+            this.lblDescrition.Text = "Enter a letter and then press the guess button.  You have 6 attempts or you lose!" +
+    "";
+            // 
+            // imgHangman
+            // 
+            this.imgHangman.Image = global::Part_8___Hangman.Properties.Resources.HangmanFresh;
+            this.imgHangman.Location = new System.Drawing.Point(12, 111);
+            this.imgHangman.Name = "imgHangman";
+            this.imgHangman.Size = new System.Drawing.Size(339, 239);
+            this.imgHangman.TabIndex = 0;
+            this.imgHangman.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 450);
+            this.Controls.Add(this.lblDescrition);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstUsedLetters);
+            this.Controls.Add(this.lblUsedLetters);
+            this.Controls.Add(this.imgHangman);
+            this.Name = "frmMain";
+            this.Text = "Hangman (Game Of The Year Edition)";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgHangman;
+        private System.Windows.Forms.Label lblUsedLetters;
+        private System.Windows.Forms.ListBox lstUsedLetters;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescrition;
+    }
+}
+
